b2771ec0d63d9f1633fc83fcf329a358cb05fcf3
SpirosVitas <167764096+SpirosVitas@users.noreply.github.com>
2025-02-03 20:33:12 +0200

    Add files via upload

---
+++ exception/TriviaAPIException.java
@@ -0,0 +1,13 @@
+package exception;
+
+public class TriviaAPIException extends Exception {
+	
+
+	public TriviaAPIException(String message) {
+		super(message);
+	}
+	public TriviaAPIException(String message, Throwable cause ) {
+		super(message, cause);
+	}
+
+}
